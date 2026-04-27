997f4fa2eb19991d26b4bb8fd4d7a21edebb712c
Yuranosuke311 <suzukiyura0311@gmail.com>
2026-04-27 22:09:53 +0900

    エラー修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["pdfkit"],
+  serverExternalPackages: ["pdfkit", "@react-pdf/renderer"],
   async headers() {
     return [
       {
